c6544ffbc8fdfe0b19f828cf6dceb92658c24247
hannybear88 <hannahkchung88@gmail.com>
2022-10-13 00:19:47 -0700

    edited code on script.js

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
+//Testing code
+
+var passwordLength = 8;
+var passwordArray = [];
+
+// Lower case alphabet lettes to use in password
+var alphaLowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// Upper case alphabet to use in password 
+var alphaUppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// Numbers to use in password
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Special symbols to use in password
+var specialCharactersArray =  [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// comment
-// Lower case alphabet lettes to use in password
-var alphaLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// Upper case alphabet to use in password 
-var alphaUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// Numbers to use in password
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Special symbols to use in password
-var Symbols = ["!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-
-//Stores what the password length is in the memory
-var passwordLength; 
-//Lines 16, 18, 20, 22, and 24 Stores the boolean response for each variable on those lines. 
-var useLower;
-// refer to comment on line 15
-var useUpper;
-// refer to comment on line 15
-var useNumbers;
-// refer to comment on line 15
-var useSpecial;
-
-var passwordOptions;
-
-var passwordArray = [];
-var password = "";
-
-var validEntry = false;
-function generatePassword(){
-// Sets up the password length based on the prompt
-validEntry = false;
-while (validEntry === false) {
-  var passwordLength = prompt("How many characters would you like to use? Please enter between 8 and 128 characters.");
- 
-  if (passwordLength < 8 || passwordLength > 128) {
-     //alert("Please enter a number between 8 and 128");
-    validEntry = false;
-  } else {
-    validEntry = true;
-    prompt = passwordLength;
-  }
-}
-}
-
-console.log("Your password will be: " + passwordLength + " chracters long.")
-
-var useSymbols = confirm("Would you like to include symbol characters in your password?");
-console.log("Using special symbol characters: " + useSymbols + ".");
-
-for (var i = 0; i < passwordLength; i++) {
-  var randomChar = pwChoices[Math.floor(Math.random() * pwOptions.length)];
-  pwArray[i] = randomChar;
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var validEntry = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+if (validEntry) {
+  var reGeneratedPassword = generatePassword();
+  passwordText.value = reGeneratedPassword;
+} else { 
+  passwordText.value = "";
+}
+}
+
+function generatePassword() {
+//Generates the password length based on the prompt
+var password = "";
+for(var i = 0; i < passwordLength; i++) {
+    var randomCharacters = Math.floor (Math.random() * passwordArray.length);
+    password = password + passwordArray[randomCharacters];
+  }
+  return password;
+}
+
+function getPrompts(){
+    passwordArray = [];
+    passwordLength = parseInt(prompt("How many characters would you like to use? Please enter between 8 and 128 characters."));
+
+    if(isNaN(passwordLength) || characterLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters. Please try again.");
+    return false;
+ }
+
+  if (confirm("Would you like to include special characters in your password?")) {
+   passwordArray = passwordArray.concat(specialCharactersArray);
+  }
+
+  if (confirm("Would you like to include numbers in your password?")) {
+   passwordArray = passwordArray.concat(numbersArray);
+  }
+
+  if (confirm("Would you like to include lowercase letters in your password?")) {
+  passwordArray = passwordArray.concat(alphaLowercaseArray);
+  }
+
+  if (confirm("Would you like to include uppercase letters in your password?")) {
+    passwordArray = passwordArray.concat(alphaUppercaseArray);
+  }
+  return true;
+}
